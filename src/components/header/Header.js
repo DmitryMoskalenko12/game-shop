ac7b39a8a6477bb7a746c2c32cec06e6622eddcc
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-12-17 01:56:05 +0200

    add asidemenu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import './header.scss';
+import { useState } from 'react';
 import logo from '../../image/logo.png';
 import lup from '../../icons/lup.png';
 import phone from '../../icons/phone.png';
@@ -6,10 +7,30 @@ import human from '../../icons/human.png';
 import basket from '../../icons/bascet.png';
 
 const Header = () => {
+  const [burger, setBurger] = useState(false);
+
   return(
     <header className='header'>
      <div className="container">
+      <div className={ burger ? "header__shadow" : ''}>
+        <ul className={burger ? "header__asidemenu asideactive": "header__asidemenu" }>
+          <li className="header__asideli">Настольные игры</li>
+          <li className="header__asideli">Warhammer 40000</li>
+          <li className="header__asideli">Magic:the Gathering</li>
+          <li className="header__asideli">Аксессуары для игр</li>
+          <li className="header__asideli">Краски</li>
+          <li className="header__asideli">Товары для детей</li>
+          <li className="header__asideli">Аксессуары для моделизма</li>
+        </ul>
+      </div>
       <ul className="header__up">
+        <li className="header__burger">
+          <div onClick={() => setBurger(burger => !burger)} className="header__burgermenu">
+            <span className={burger ? "header__burgerline active": "header__burgerline"}></span>
+            <span className={burger ? "header__burgerline active": "header__burgerline"}></span>
+            <span className={burger ? "header__burgerline active": "header__burgerline"}></span>
+          </div>
+        </li>
         <li className="header__logo">
           <div className="header__wraplogo">
             <a href="#"><img src={logo} alt="logo" /></a>
