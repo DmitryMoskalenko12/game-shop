a6ff2b5bffcddd33f7677cc4e9922d307d5ea2dc
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-12-21 01:18:06 +0200

    add active class for link in menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import './header.scss';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import logo from '../../image/logo.png';
 import lup from '../../icons/lup.png';
 import phone from '../../icons/phone.png';
@@ -8,33 +8,44 @@ import basket from '../../icons/bascet.png';
 
 const Header = () => {
   const [burger, setBurger] = useState(false);
+  const [activeLi, setActiveLi] = useState(null)
   const asideLiUp = [
-    {clas: 'header__asideli', href: "#", content: 'Настольные игры'},
-    {clas: 'header__asideli', href: "#", content: 'Warhammer 40000'},
-    {clas: 'header__asideli', href: "#", content: 'Magic:the Gathering'},
-    {clas: 'header__asideli', href: "#", content: 'Аксессуары для игр'},
-    {clas: 'header__asideli', href: "#", content: 'Краски'},
-    {clas: 'header__asideli', href: "#", content: 'Товары для детей'},
-    {clas: 'header__asideli', href: "#", content: 'Аксессуары для моделизма'}
+    {clas: 'header__asideli', href: "#", content: 'Настольные игры', id: 1},
+    {clas: 'header__asideli', href: "#", content: 'Warhammer 40000', id: 2},
+    {clas: 'header__asideli', href: "#", content: 'Magic:the Gathering', id: 3},
+    {clas: 'header__asideli', href: "#", content: 'Аксессуары для игр', id: 4},
+    {clas: 'header__asideli', href: "#", content: 'Краски', id: 5},
+    {clas: 'header__asideli', href: "#", content: 'Товары для детей', id: 6},
+    {clas: 'header__asideli', href: "#", content: 'Аксессуары для моделизма', id: 7}
   ]
+  const asideLiDown = [
+    {clas: 'header__asideli', href: "#", content: 'Мероприятия'},
+    {clas: 'header__asideli', href: "#", content: 'Блог'},
+    {clas: 'header__asideli', href: "#", content: 'О центре'},
+    {clas: 'header__asideli', href: "#", content: 'Контакты'}
+  ]
+
+  const onActive =(e) => {
+    setActiveLi(e.currentTarget.textContent)
+   }
+
   return(
     <header className='header'>
      <div className="container">
       <div className={burger ? "header__shadow" : ''}>
         <ul className={burger ? "header__asidemenu asideactive" : "header__asidemenu" }>
          {
-          asideLiUp.map(({clas, href, content}) => {
-            return  <li className={clas}><a href={href}>{content}</a></li>
+          asideLiUp.map(({clas, href, content, id}, i) => {
+            return  <li key={i} onClick={(e) => onActive(e)} className={`${clas} ${(activeLi === content) ? 'activeli' : ''}`}><a href={href}>{content}</a></li>
           })
          }
-    
           <li  className='header__hr'></li>
-          <li className="header__asideli"><a href="#">Мероприятия</a></li>
-          <li className="header__asideli"><a href="#">Блог</a></li>
-          <li className="header__asideli"><a href="#">О центре</a></li>
-          <li className="header__asideli"><a href="#">Контакты</a></li>
+        {
+          asideLiDown.map(({clas, href, content}, i) => {
+          return <li onClick={(e) => onActive(e)} key={i} className={`${clas} ${(activeLi === content) ? 'activeli' : ''}`}><a href={href}>{content}</a></li>
+          })
+        } 
         </ul>
-    
       </div>
       <ul className="header__up">
         <li className="header__burger">
