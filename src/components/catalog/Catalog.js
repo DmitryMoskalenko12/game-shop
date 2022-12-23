6d83986135566f2b52b6c586d7e4e758ab49e466
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-12-24 01:24:13 +0200

    add catalog functional

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import './catalog.scss';
 
 const Catalog = (props) => {
+  const [activeLink, setActiveLink] = useState(null);
+
   const {setModal} = props;
+  const catalogLink = [
+    {clazz: 'catalog__link start', href:'#', content: 'Настольные игры', id: 1},
+    {clazz: 'catalog__link start', href:'#', content: 'Warhammer 40000', id: 2},
+    {clazz: 'catalog__link start', href:'#', content: 'Magic: the Gathering', id: 3},
+    {clazz: 'catalog__link start', href:'#', content: 'Аксессуары для игр', id: 4},
+    {clazz: 'catalog__link start', href:'#', content: 'Краски', id: 5},
+    {clazz: 'catalog__link start', href:'#', content: 'Товары для детей', id: 6},
+    {clazz: 'catalog__link start', href:'#', content: 'Аксессуары для моделизма', id: 7},
+  ]
+
+  const onActive =(e) => {
+    setActiveLink(e.currentTarget.textContent)
+   }
 
   return(
     <nav className='catalog'>
@@ -11,13 +27,11 @@ const Catalog = (props) => {
          <div className="catalog__categoruclose">Все категории</div>
         </li>
         <li className='catalog__hr'></li>
-        <li className="catalog__link start">Настольные игры</li>
-        <li className="catalog__link start">Warhammer 40000</li>
-        <li className="catalog__link start">Magic: the Gathering</li>
-        <li className="catalog__link start">Аксессуары для игр</li>
-        <li className="catalog__link start">Краски</li>
-        <li className="catalog__link start">Товары для детей</li>
-        <li className="catalog__link start">Аксессуары для <br /> моделизма</li>
+        {
+          catalogLink.map(({content, href, clazz, id}, i) => {
+            return <li onClick={(e) => onActive(e)} style = {{background: content === activeLink  ? '#F9A43F': null}} key={i} className={clazz}><a href={href}>{content}</a></li>
+          })
+        }
       </ul>
     </nav>
   )
