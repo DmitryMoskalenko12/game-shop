180494d472ae7ca332332828b760fcb4bafbd0d1
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-12-25 23:01:08 +0200

    add db and http.hook

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -1,8 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './catalog.scss';
+import useHttp from '../../hooks/http.hook';
 
 const Catalog = (props) => {
-  const [activeLink, setActiveLink] = useState(null);
+  const [activeLink, setActiveLink] = useState('Warhammer 40000');
+  const {request} = useHttp();
+
+  useEffect(() => {
+    localStorage.setItem('active', activeLink)
+  },[activeLink])
+
+  useEffect(() => {
+   request('http://localhost:3001/warhammer')
+   .then((res) => console.log(res))
+  },[])
 
   const {setModal} = props;
   const catalogLink = [
@@ -12,10 +23,10 @@ const Catalog = (props) => {
     {clazz: 'catalog__link start', href:'#', content: 'Аксессуары для игр', id: 4},
     {clazz: 'catalog__link start', href:'#', content: 'Краски', id: 5},
     {clazz: 'catalog__link start', href:'#', content: 'Товары для детей', id: 6},
-    {clazz: 'catalog__link start', href:'#', content: 'Аксессуары для моделизма', id: 7},
+    {clazz: 'catalog__link start', last: 'last', href:'#', content: 'Аксессуары для моделизма', id: 7},
   ]
 
-  const onActive =(e) => {
+  const onActive = (e) => {
     setActiveLink(e.currentTarget.textContent)
    }
 
@@ -28,8 +39,8 @@ const Catalog = (props) => {
         </li>
         <li className='catalog__hr'></li>
         {
-          catalogLink.map(({content, href, clazz, id}, i) => {
-            return <li onClick={(e) => onActive(e)} style = {{background: content === activeLink  ? '#F9A43F': null}} key={i} className={clazz}><a href={href}>{content}</a></li>
+          catalogLink.map(({content, href, clazz, last}, i) => {
+            return <li onClick={(e) => {onActive(e); setActiveLink(content); localStorage.setItem('active', content)}} style = {{background: localStorage.getItem('active') === content ? '#F9A43F': null}} key={i} className={clazz}><a className={last} href={href}>{content}</a></li>
           })
         }
       </ul>
@@ -37,3 +48,5 @@ const Catalog = (props) => {
   )
 }
 export default Catalog;
+
+
