8bcbd6049cae7e64bd194d16e1a0de77d9e1bcde
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-12-31 01:50:11 +0200

    add carousel

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,16 @@
+import './carousel.scss';
+
+const Carousel = () => {
+  return(
+    <div className='carousel'>
+      <div style={{overflow: 'hidden'}} className="carousel__window">
+        <div className="carousel__field">
+
+        
+        </div>
+      </div>
+    </div>
+
+  )
+}
+export default Carousel;
---
+++ src/components/carousel/carouselSlice.js
@@ -0,0 +1,10 @@
+import { createSlice, createAsyncThunk, createSelector } from "@reduxjs/toolkit";
+
+const initialState = {
+  data: [],
+  status:'idle'
+}
+
+const carousel = createSlice({
+  
+})
